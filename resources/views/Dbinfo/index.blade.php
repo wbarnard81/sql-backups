c6e431f6228be2228befa1dab8561e294659b919
Werner Barnard <werner.barnard@vaimo.com>
2023-09-18 14:53:32 +0200

    first commit

---
+++ resources/views/Dbinfo/index.blade.php
@@ -0,0 +1,18 @@
+<x-app-layout>
+  <x-slot name="header">
+    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+      {{ __('Dashboard') }}
+    </h2>
+  </x-slot>
+
+  <div class="py-12">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+      <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+        <div class="p-4">
+          <a href="/dbinfo/add" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Add</a>
+          <p class="py-2">DB Info Goes here.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</x-app-layout>
---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,20 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <div>
+                    <p>Text goes here.</p>
+                    <div>
+                        <a href="/dbinfo">View DB Info</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
